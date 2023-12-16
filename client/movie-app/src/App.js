b3495c9de666d179e423e669b780b597bcc5beb7
choilettuce <tkdgnstkdcn@naver.com>
2023-12-16 19:43:13 +0900

    feat : login3

--- client/movie-app/src/App.js
+++ client/movie-app/src/App.js
@@ -3,8 +3,9 @@ import Home from "./components/Home/Home";
 import Detail from "./components/Detail/Detail";
 import Header from "./components/Header/header";
 import "./App.css";
-import Login from "./components/login/login";
+// import Login from "./components/login/login";
 // import Login2 from "./components/login/login2";
+import Login3 from "./components/login/login3";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Routes >
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element ={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login3 />} />
       </Routes>
      </BrowserRouter>
   );
