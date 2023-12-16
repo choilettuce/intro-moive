a0f5d92b1d6c0cbb802c8187b1a51b1465c4eea3
choilettuce <tkdgnstkdcn@naver.com>
2023-12-16 19:09:17 +0900

    feat : login2

--- client/movie-app/src/App.js
+++ client/movie-app/src/App.js
@@ -4,6 +4,7 @@ import Detail from "./components/Detail/Detail";
 import Header from "./components/Header/header";
 import "./App.css";
 import Login from "./components/login/login";
+// import Login2 from "./components/login/login2";
 
 function App() {
   return (
